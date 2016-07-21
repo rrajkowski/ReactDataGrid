e4b2717a498868a37aa161f155b71e60e8891cd3
Ruben Rajkowski <rajkowskiruben@NUBBURIT569AEML.local>
2016-07-20 18:59:32 -0700

    [master] update components for JSX

--- js/main.js
+++ js/main.js
@@ -1,59 +1,46 @@
 /* ReactJS Table w/o JSX/Babel  */
 'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }return call && ((typeof call === "undefined" ? "undefined" : _typeof(call)) === "object" || typeof call === "function") ? call : self;
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function, not " + (typeof superClass === "undefined" ? "undefined" : _typeof(superClass)));
+  }subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+}
 
 var FixedTable = function (_React$Component) {
   _inherits(FixedTable, _React$Component);
 
-  function FixedTable(props) {
-    _classCallCheck(this, FixedTable);
+  function FixedTable(){};
 
-    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
-
-    _this.state = {
-      items: props.items,
+  FixedTable.prototype.componentWillMount = function componentWillMount() {
+    this.setState({
+      data: this.props.data,
+      filteredData: this.props.data,
       sortingProp: 'id',
       sortingDirectionAsc: true
-    };
-
-    _this.scrollSidebar = _this.scrollSidebar.bind(_this);
-    _this.scrollBodyListHeader = _this.scrollBodyListHeader.bind(_this);
-    return _this;
-  }
+    });
+  };
 
   FixedTable.prototype.componentDidMount = function componentDidMount() {
-    var _this2 = this;
 
-    var $fixedTableBodyList = ReactDOM.findDOMNode(this.refs.fixedTableBodyList);
-
-    Ps.initialize($fixedTableBodyList);
-
-    $fixedTableBodyList.addEventListener('ps-scroll-y', this.scrollSidebar);
-    $fixedTableBodyList.addEventListener('ps-y-reach-start', function () {
-      return _this2.scrollSidebar({ target: { scrollTop: 0 } });
-    });
-    $fixedTableBodyList.addEventListener('ps-scroll-x', this.scrollBodyListHeader);
-    $fixedTableBodyList.addEventListener('ps-x-reach-start', function () {
-      return _this2.scrollBodyListHeader({ target: { scrollLeft: 0 } });
-    });
   };
 
-  FixedTable.prototype.componentWillReceiveProps = function componentWillReceiveProps(nextProps) {
-    this.setState({
-      data: nextProps.items,
-      filteredItems: Immutable.List(),
-      sortingProp: 'id',
-      sortingDirectionAsc: true
-    });
-  };
+  FixedTable.prototype.componentWillReceiveProps = function componentWillReceiveProps() {
 
-  FixedTable.prototype.scrollSidebar = function scrollSidebar(event) {
-    ReactDOM.findDOMNode(this.refs.sidebarList).style.top = -event.target.scrollTop + 'px';
   };
 
   FixedTable.prototype.scrollBodyListHeader = function scrollBodyListHeader(event) {
@@ -61,7 +48,8 @@ var FixedTable = function (_React$Component) {
   };
 
   FixedTable.prototype.sortAsc = function sortAsc(prop) {
-    return this.state.items.sort(function (a, b) {
+    var data  = (this.state.data.length > this.state.filteredData.length) ? this.state.filteredData :  this.state.data;
+    return data.sort(function (a, b) {
       if (a[prop] > b[prop]) {
         return 1;
       }
@@ -75,7 +63,8 @@ var FixedTable = function (_React$Component) {
   };
 
   FixedTable.prototype.sortDesc = function sortDesc(prop) {
-    return this.state.items.sort(function (a, b) {
+    var data  = (this.state.data.length > this.state.filteredData.length) ? this.state.filteredData :  this.state.data;
+    return data.sort(function (a, b) {
       if (a[prop] < b[prop]) {
         return 1;
       }
@@ -88,18 +77,20 @@ var FixedTable = function (_React$Component) {
     });
   };
 
+
+  // Method to Sort table columns
   FixedTable.prototype.sortBy = function sortBy(prop) {
     if (prop === this.state.sortingProp) {
       if (this.state.sortingDirectionAsc) {
         this.setState({
           sortingDirectionAsc: false,
-          items: this.sortDesc(prop)
+          data: this.sortDesc(prop)
         });
         return;
       } else {
         this.setState({
           sortingDirectionAsc: true,
-          items: this.sortAsc(prop)
+          data: this.sortAsc(prop)
         });
         return;
       }
@@ -107,124 +98,188 @@ var FixedTable = function (_React$Component) {
     this.setState({
       sortingProp: prop,
       sortingDirectionAsc: true,
-      items: this.sortAsc(prop)
+      data: this.sortAsc(prop)
+    });
+  };
+
+
+  // Method to Filter table row by column id
+  FixedTable.prototype.filterBy = function filterBy(event) {
+    event.preventDefault();
+    var _this = this,
+        value = event.target.value,
+        filteredData = [],
+        filterMap = {},
+        list = Immutable.fromJS(this.state.data.length > this.state.filteredData.length ? this.state.filteredData :  this.state.data),
+        $filterInput = $(".fixed-table__filterable-header input[type=text]"),
+        len = $filterInput.length;
+
+    // rest map if filter is removed
+    if(value === ''){
+      filterMap = {};
+      list = Immutable.fromJS(this.state.data);
+    }
+
+    // onchange on every field
+    $filterInput.each(function (index) {
+      var val = $(this).val(),
+          id = this.id;
+      if (val !== '' && id.length) {
+        filterMap[id] = new RegExp(val, 'i');
+
+        for(var f in filterMap) {
+          filteredData = list.filter(function (item) {
+              var filterItem = item.get(f);
+              if(!filterItem) console.warn('[MPS Tables] malformed or missing filter id: ',id);
+              if (filterItem && filterItem.toString().search(filterMap[f]) > -1) {
+                return filterItem;
+              }
+          });
+        }
+      }
+    });
+
+
+    // update filtered state
+    this.setState({
+      sortingProp: this.sortingProp,
+      sortingDirectionAsc: this.sortingDirectionAsc,
+      filteredData: $.map(filteredData.toJS(), function(val, i) { return [val]; })
+    });
+
+  };
+
+
+  // Clear filters
+  FixedTable.prototype.clearFilter = function clearFilter(event){
+    event.preventDefault();
+    console.log('[MPS Tables] Clear Table Filter',this.state.filteredData);
+    $(".fixed-table__filterable-header input[type=text]").val('');
+    this.setState({
+      data: this.props.data,
+      filteredData: this.props.data,
+      sortingProp: 'id',
+      sortingDirectionAsc: true
     });
   };
 
 
   FixedTable.prototype.renderList = function renderList() {
-    var items = this.state.items.map(function (i, index) {
-      return React.createElement(
-          'div',
-          { key: index,
-            className: 'fixed-table__row' },
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.id
-          ),
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.first_name
-          ),
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.last_name
-          ),
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.email
-          ),
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.country
-          ),
-          React.createElement(
-              'div',
-              { className: 'fixed-table__col' },
-              i.ip_address
-          )
-      );
+    var data  = (this.state.data.length > this.state.filteredData.length) ? this.state.filteredData :  this.state.data;
+    var rowheaders = data.map(function(i, index) {
+      return React.createElement('div', {
+            key: index,
+            className: 'fixed-table__row'
+          },
+          React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.id),
+          React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.first_name), React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.last_name), React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.email), React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.country), React.createElement('div', {
+            className: 'fixed-table__col'
+          }, i.ip_address));
     });
-    return React.createElement(
-        'div',
-        { ref: 'fixedTableBodyList',
-          className: 'fixed-table__list'},
-        items
-    );
+    return React.createElement('div', { ref: 'fixedTableBodyList',
+      className: 'fixed-table__list' }, rowheaders);
   };
+
 
   FixedTable.prototype.render = function render() {
-    return React.createElement(
-        'div',
-        { className: 'fixed-table' },
-
-        React.createElement(
-            'div',
-            { className: 'fixed-table__body' },
-            React.createElement(
-                'div',
-                { className: 'fixed-table__header' },
-                React.createElement(
-                    'div',
-                    { ref: 'bodyListHeader',
-                      className: 'fixed-table__scrollable-header' },
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'id') },
-                        'Id'
-                    ),
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'first_name') },
-                        'First Name'
-                    ),
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'last_name') },
-                        'Last Name'
-                    ),
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'email') },
-                        'Email'
-                    ),
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'country') },
-                        'Country'
-                    ),
-                    React.createElement(
-                        'div',
-                        { className: 'fixed-table__th',
-                          onClick: this.sortBy.bind(this, 'ip_address') },
-                        'Ip Address'
-                    )
-                )
-            ),
-            this.renderList()
-        )
-    );
+  return React.createElement('div', {
+    className: 'fixed-table'
+  }, React.createElement('div', {
+      className: 'fixed-table__body'
+    }, React.createElement('div', {
+        className: 'fixed-table__header'
+      }, React.createElement('div', {
+        ref: 'bodyListHeader',
+        className: 'fixed-table__scrollable-header'
+        }, React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'id')
+        }, 'Id'), React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'first_name')
+        }, 'First Name'), React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'last_name')
+        }, 'Last Name'), React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'email')
+        }, 'Email'), React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'country')
+        }, 'Country'), React.createElement('div', {
+          className: 'fixed-table__th',
+          onClick: this.sortBy.bind(this, 'ip_address')
+        }, 'Ip Address')),
+      React.createElement('div', {
+        ref: 'bodyFilterHeader',
+        className: 'fixed-table__filterable-header'
+        }, React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__fh',
+          placeholder: 'Id',
+          id: 'id',
+          onChange: this.filterBy.bind(this)
+        }), React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__fh',
+          placeholder: 'First Name',
+          id: 'first_name',
+          onChange: this.filterBy.bind(this)
+        }), React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__th',
+          placeholder: 'Last Name',
+          id: 'last_name',
+          onChange: this.filterBy.bind(this)
+        }), React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__fh',
+          placeholder: 'Email',
+          id: 'email',
+          onChange: this.filterBy.bind(this)
+        }), React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__fh',
+          placeholder: 'Country',
+          id: 'country',
+          onChange: this.filterBy.bind(this)
+        }), React.createElement('input', {
+          type: 'text',
+          className: 'fixed-table__fh',
+          placeholder: 'IP Address',
+          id: 'ip_address',
+          onChange: this.filterBy.bind(this)
+        }),
+        React.createElement('input', {
+          type: 'button',
+          value: 'Clear',
+          className: 'fixed-table__fh btn',
+          onClick: this.clearFilter.bind(this)
+        }))),
+    this.renderList()
+    ));
   };
-
   return FixedTable;
 }(React.Component);
 
 FixedTable.propTypes = {
-  data: React.PropTypes.array.isRequired
+  data: React.PropTypes.array.isRequired,
+  filteredData: React.PropTypes.array
 };
 
 
-$.get('data/items.json', function (data) {
-  ReactDOM.render(React.createElement(FixedTable, { data: data }), document.querySelector('#container'));
-  //console.log(JSON.stringify(items));
+// get JSON array from file/endpoint
+$.get('data/data.json', function (data) {
+  ReactDOM.render(React.createElement(FixedTable, { data: data, filteredData: Immutable.fromJS(data) }), document.querySelector('#container'));
+  //console.log(JSON.stringify(data));
 });
---
+++ js/tableview.js
@@ -0,0 +1,198 @@
+/* ReactJS Table JSX Transpiler (Bable)  */
+'use strict';
+
+
+// Clear filters
+function clearFilter(event){
+  event.preventDefault();
+  console.log('[MPS Tables] Clear Table Filter',this.state.filteredData);
+  $(".fixed-table__filterable-header input[type=text]").val('');
+  this.setState({
+    data: this.props.data,
+    filteredData: this.props.data,
+    sortingProp: 'id',
+    sortingDirectionAsc: true
+  });
+}
+
+
+var TableView = React.createClass({
+  getInitialeState: function() {
+    this.state = {
+      data: Immutable.List(),
+      filteredData: Immutable.List(),
+      sortingProp: 'id',
+      sortingDirectionAsc: true
+    }
+  },
+  componentWillMount: function() {
+    this.setState({
+      data: this.props.data,
+      filteredData: this.props.data,
+      sortingProp: 'id',
+      sortingDirectionAsc: true
+    });
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      data: this.nextProps.data
+    });
+  },
+  statics: {
+    // Custom event handlers
+    sortBy: function (prop) {
+      console.log('sortBy: ', prop);
+      // TODO: replace $r with the TableView 'state'
+      if (prop === $r.state.sortingProp) {
+        if ($r.state.sortingDirectionAsc) {
+          $r.setState({
+            sortingDirectionAsc: false,
+            data: TableView.sortDesc(prop)
+          });
+          return;
+        } else {
+          $r.setState({
+            sortingDirectionAsc: true,
+            data: TableView.sortAsc(prop)
+          });
+          return;
+        }
+      }
+      $r.setState({
+        sortingProp: prop,
+        sortingDirectionAsc: true,
+        data: TableView.sortAsc(prop)
+      });
+    },
+    // Method to sort Ascending
+    sortAsc: function(prop) {
+      var data  = ($r.state.data.length > $r.state.filteredData.length) ? $r.state.filteredData :  $r.state.data;
+      return data.sort(function (a, b) {
+        if (a[prop] > b[prop]) {
+          return 1;
+        }
+
+        if (a[prop] < b[prop]) {
+          return -1;
+        }
+        return 0;
+      });
+    },
+
+    // Method to sort Descending
+    sortDesc: function(prop) {
+      var data  = ($r.state.data.length > $r.state.filteredData.length) ? $r.state.filteredData :  $r.state.data;
+      return data.sort(function (a, b) {
+        if (a[prop] < b[prop]) {
+          return 1;
+        }
+
+        if (a[prop] > b[prop]) {
+          return -1;
+        }
+
+        return 0;
+      });
+    },
+    // Method to Filter table row by column id
+    filterBy: function(event) {
+      event.preventDefault();
+      var _this = this,
+          value = event.target.value,
+          filteredData = [],
+          filterMap = {},
+          list = Immutable.fromJS($r.state.data.length > $r.state.filteredData.length ? $r.state.filteredData :  $r.state.data),
+          $filterInput = $(".fixed-table__filterable-header input[type=text]");
+      console.warn('filterBy: ',value, value.length);
+      // rest map if filter is removed
+      if(value === ''){
+        filterMap = {};
+        list = Immutable.fromJS($r.state.data);
+      }
+
+      // onchange on every field
+      $filterInput.each(function (index) {
+        var val = $(this).val(),
+            id = this.id;
+        if (val !== '' && id.length) {
+          filterMap[id] = new RegExp(val, 'i');
+
+          for(var f in filterMap) {
+            filteredData = Immutable.fromJS($r.state.filteredData).filter(function (item) {
+              var filterItem = item.get(f);
+              if(!filterItem) console.warn('[MPS Tables] malformed or missing filter id: ',id);
+              if (filterItem && filterItem.toString().search(filterMap[f]) > -1) {
+                return filterItem;
+              }
+            });
+          }
+        }
+      });
+
+      // update filtered state
+      $r.setState({
+        sortingProp: $r.sortingProp,
+        sortingDirectionAsc: $r.sortingDirectionAsc,
+        filteredData: $.map(filteredData.toJS(), function(val, i) { return [val]; })
+      });
+    }
+  },
+  render: function() {
+    //var data  = (this.state.data.length > this.state.filteredData.length) ? this.state.filteredData :  this.state.data;
+    console.log('data:',this.state.data.length);
+    return(
+        <div className="fixed-table" >
+        <div className="fixed-table__body" >
+        <FixedTableHeader />
+        <div className="fixed-table__list" >
+        {this.state.data.map(function(item, i) {
+          return(
+           <div key={i} className="fixed-table__row" >
+           <div className="fixed-table__col" >{ item.id }</div>
+           <div className="fixed-table__col" >{ item.first_name }</div>
+           <div className="fixed-table__col" >{ item.last_name }</div>
+           <div className="fixed-table__col" >{ item.email }</div>
+           <div className="fixed-table__col" >{ item.country }</div>
+           <div className="fixed-table__col" >{ item.ip_address }</div>
+           </div>)
+        })}
+      </div>
+      </div>
+      </div>)
+  }
+});
+
+var FixedTableHeader = React.createClass({
+  render: function() {
+    return (
+
+          <div className="fixed-table__header" >
+            <div className="fixed-table__title-header" >
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'id')} >Id</div>
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'first_name')} >First Name</div>
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'last_name')} >Last Name</div>
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'email')} >Email</div>
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'country')} >Country</div>
+              <div className="fixed-table__th" onClick={TableView.sortBy.bind(this, 'ip_address')} >Ip address</div>
+            </div>
+            <div className="fixed-table__filterable-header" >
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="Id" type="text" id="id"/>
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="First Name" type="text" id="first_name"/>
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="Last Name" type="text" id="last_name"/>
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="Email" type="text" id="email"/>
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="Country" type="text" id="country"/>
+              <input className="fixed-table__fh" onChange={TableView.filterBy.bind(this)} placeholder="Ip address" type="text" id="ip_address"/>
+            </div>
+        </div>
+    )
+  }
+});
+
+$.get('data/data.json', function (data) {
+  ReactDOM.render(
+        <TableView data={data} />,
+    document.getElementById('container')
+  );
+}).fail(function(err) {
+  console.warn('[MPS Tables] Failed to fetch data: ', err.status + ':' + err.statusText);
+});
